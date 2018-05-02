c80dea49dbb5af352dd2a28c3aa153d2ce3b885c
Alexandria Brown <axbn2190@gmail.com>
2018-05-02 16:20:47 -0700

    lib files created

---
+++ test/lib/edamam_api_wrapper_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+describe EdamamApiWrapper do
+  it "can list all recipes from a given search" do
+
+  end 
+
+
+end
---
+++ test/lib/recipe_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+describe Recipe do
+  it "Raises an error if params are missing" do
+
+  end
+
+  it "Must initialize params properly" do
+
+  end
+
+end

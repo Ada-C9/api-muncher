d869cfd3c05b1cb8e1e9a01f64c6021b094a3a17
Phoebe <phoebe.manning8@gmail.com>
2018-05-02 15:26:52 -0700

    updates Gemfile and adds vcr logic in application.rb

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 describe RecipesController do
-  # describe "root" do
-  #   get root_path
-  #   must_respond_with :success
-  # end
+  let(:valid_search) { "cake" }
+
+  describe "root" do
+    it "succeeds" do
+      get root_path
+      must_respond_with :success
+    end
+  end
+
+  describe "list" do
+    it "succeeds with valid search text" do
+      VCR.use_cassette("recipe") do
+        search = valid_search
+        response = RecipeSearchWrapper.list_recipes(search)
+        binding.pry
+        # response["ok"].must_equal true
+        # response["message"]["text"].must_equal message
+      end
+    end
+  end
 
 end

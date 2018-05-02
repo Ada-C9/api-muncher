7dbb5713f1f84046a100e6d275f5e39446241779
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-05-02 11:45:56 -0700

    Added routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'recipes#search'
+  get '/results', to: 'recipes#index', as: 'results'
+  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
 end

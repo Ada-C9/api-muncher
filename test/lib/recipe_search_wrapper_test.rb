9ef4ed46baf1304517113e3f83f7fe8b3e62d72a
Anne <annehwatson@gmail.com>
2018-05-02 11:59:11 -0700

    first wrapper test

---
+++ test/lib/recipe_search_wrapper_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+describe "RecipeApiWrapper" do
+
+  describe "search_recipes" do
+
+    it "gives a list of recipes" do
+      VCR.use_cassette("recipes") do
+        recipes = RecipeSearchWrapper.search_recipes
+
+        recipes.each do |recipe|
+          recipe.must_be_kind_of Recipe
+        end
+      end
+    end
+    
+  end
+end

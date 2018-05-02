ff18018160d53a3e6789d8979872d10d972020cc
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-02 15:44:40 -0700

    Write Recipes controller 'index' action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,11 @@
 class RecipesController < ApplicationController
+
+  def index
+    search_term = params[:search]
+    @recipes = EdamamApiWrapper.list_recipes(search_term)
+  end
+
+  def show
+  end
+
 end

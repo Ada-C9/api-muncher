21f69c08c81d5844f24eef88e2a1de946a8e8e7a
knockknockhusthere <angela.huxia@gmail.com>
2018-05-02 00:21:09 -0700

    created recipe controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'recipe#index'
+
+  resources :recipes
+  # get ':channel/new', to: 'chat#new', as: 'new_message'
+  # post ':channel', to: 'chat#create', as: 'send_message'
 end

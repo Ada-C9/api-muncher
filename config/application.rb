c61c79ca2d9953ea12040ea4a1e90c49f7f6c276
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-01 17:52:23 -0700

    added dotenv to gemfile, updated application.rb to handle lib file

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module ApiMuncher
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # Automatically load all code from <rails_root>/lib
+    config.autoload_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

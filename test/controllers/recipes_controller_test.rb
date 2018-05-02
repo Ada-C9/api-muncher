fbfb6acda4e0ed1df7fa021d6adedf7602d0f5b7
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-05-02 15:30:08 -0700

    add recipe controller

---
+++ test/controllers/recipes_controller_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+describe RecipesController do
+  it "should get index" do
+    get recipes_index_url
+    value(response).must_be :success?
+  end
+
+  it "should get show" do
+    get recipes_show_url
+    value(response).must_be :success?
+  end
+
+  it "should get root" do
+    get recipes_root_url
+    value(response).must_be :success?
+  end
+
+end

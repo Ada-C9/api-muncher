75a4ab0ab4233479c2a010d0bf6f30854f44a42c
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-02 15:42:01 -0700

    Add private create_recipe method and modify list_recipes method to use recipe class

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -6,7 +6,25 @@ class EdamamApiWrapper
   def self.list_recipes(search_term)
     url = BASE_URL + "?q=#{ search_term }"
     data = HTTParty.get(url)
-    return data
+    recipe_list = []
+    if data['hits']
+      data['hits'].each do |recipe_data|
+        recipe_list << create_recipe(recipe_data)
+      end
+    end
+    return recipe_list
+  end
+
+  private
+  def self.create_recipe(api_params)
+    recipe = api_params['recipe']
+    return Recipe.new(
+      recipe['label'],
+      recipe['image'],
+      recipe['ingredientLines'],
+      recipe['dietLabels'],
+      recipe['url']
+    )
   end
 
 end # EdamamApiWrapper

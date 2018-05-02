fce03f0ce370f51ebd16d7ac0adb4947c1686d19
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-05-02 16:03:40 -0700

    Added in a API_Wrapper, and search recipe method

---
+++ lib/edemam_api_wrapper.rb
@@ -0,0 +1,17 @@
+require 'httparty'
+
+class EdemamApiWrapper
+  BASE_URL = "https://api.edamam.com "
+  TOKEN = ENV["EDEMAM_TOKEN"]
+  API_ID = ENV["EDEMAM_ID"]
+
+  def self.search_recipes(item)
+    url = BASE_URL + "/search?q=#{item}&app_id=#{API_ID}&app_key=#{TOKEN}&from=0&to=9"
+    response = HTTParty.get(url)
+    if data["item"]
+      return data["item"]
+    else
+      return []
+    end
+  end
+end

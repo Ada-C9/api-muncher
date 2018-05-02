21a09ce6083cc040c4ae6fb8d393a22be2a7f233
Anne <annehwatson@gmail.com>
2018-05-02 10:07:36 -0700

    adding argError for label in constructor

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,7 +1,12 @@
 class Recipe
+  #change this to be like http://eric-price.net/blog/rails-api-wrapper/
   attr_reader :label
 
   def initialize(label)
+    if label.nil? || label.empty?
+      raise ArgumentError.new("The recipe needs a label")
+    end
+    
     @label = label
   end
 

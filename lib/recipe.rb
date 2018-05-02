c985e2b7be905cf106862c3f8f7d9071421fef3a
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-05-02 14:49:00 -0700

    created recipe controller and added config path

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,3 +1,13 @@
 
 class Recipe
+  attr_reader :label, :uri, :image, :ingredients, :dietary_info
+
+  def initialize(label, uri, options = { })
+    @label = label
+    @uri = uri
+    @image = options[:image]
+    @ingredient = options [:ingredients]
+    @dietary_info = options[:dietary_info]
+
+  end
 end

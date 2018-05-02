31da35a4f89779623bd4886160ee6b040812315c
Anne <annehwatson@gmail.com>
2018-05-02 12:23:17 -0700

    ingredients. all this should go on the show page eventually

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,20 +1,22 @@
 class Recipe
   #change this to be like http://eric-price.net/blog/rails-api-wrapper/
-  attr_reader :label, :url
+  attr_reader :label, :url, :ingredients
 
-  def initialize(label, url)
+  def initialize(label, url, ingredients)
     if label.nil? || label.empty?
       raise ArgumentError.new("The recipe needs a label")
     end
 
     @label = label
     @url = url
+    @ingredients = ingredients
   end
 
   def self.from_api(raw_recipe)
     self.new(
       raw_recipe["recipe"]["label"],
-      raw_recipe["recipe"]["url"]
+      raw_recipe["recipe"]["url"],
+      raw_recipe["recipe"]["ingredientLines"]
     )
   end
 end

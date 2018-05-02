721727f9d941ef26e8e2c1175d7b670a585680ea
Zheng Cao <chori84@gmail.com>
2018-05-02 16:00:44 -0700

    Config VCR tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 require "minitest/reporters"  # for Colorized output
+require 'vcr'
+require 'webmock/minitest'
 
 #  For colorful output!
 Minitest::Reporters.use!(
@@ -10,6 +12,23 @@ Minitest::Reporters.use!(
   ENV,
   Minitest.backtrace_filter
 )
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes' # folder where casettes will be located
+  config.hook_into :webmock # tie into this other tool called webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,    # record new data when we don't have it yet
+    :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
+  }
+  # Don't leave our Slack token lying around in a cassette file.
+  config.filter_sensitive_data("<EDAMAM_ID>") do
+    ENV['EDAMAM_ID']
+  end
+
+  config.filter_sensitive_data("<EDAMAM_KEYS>") do
+    ENV['EDAMAM_KEYS']
+  end
+end
 
 # https://github.com/rails/rails/issues/31324
 if ActionPack::VERSION::STRING >= "5.2.0"

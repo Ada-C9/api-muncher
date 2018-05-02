c32e3aa2df44b5e37e0bff35046c2e0e0fbd6505
Zheng Cao <chori84@gmail.com>
2018-05-01 21:30:39 -0700

    Implement basic view for home and list pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,8 +3,16 @@ class RecipesController < ApplicationController
   end
 
   def index
+    if params[:term]
+      @recipes = EdamamApiWrapper.list_recipes(params[:term])
+      session[:term] = params[:term]
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Search failed"
+    end
   end
 
   def show
   end
+
 end

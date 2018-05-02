ca08d0859b2b072d83eecba3ed355c0355aa20e5
Kaitlin Forsman <kcforsman@outlook.com>
2018-05-02 16:55:10 -0700

    wrote first test for the edamam api wrapper and got it to pass which generated a cassette

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -4,7 +4,16 @@ class EdamamApiWrapper
   ID = ENV["APPLICATION_ID"]
   KEY = ENV["APPLICATION_KEY"]
 
-  def self.list_recipes(key_words)
+  def self.list_recipes(key_words, search_range = [])
+    encoded_key_words = URI.encode(key_words)
+    response = HTTParty.get("#{BASE_URL}q=#{encoded_key_words}&app_id=#{ID}&app_key=#{KEY}")
+    recipe_list = []
+    if response["hits"]
+      response["hits"].each do | recipe |
+        recipe_list << recipe
+      end
+    end
+    return recipe_list
   end
 
   # def self.return_recipe

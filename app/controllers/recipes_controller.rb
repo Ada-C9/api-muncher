76d72a771f48ebbf1d03551a8daa599e48f883c1
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-02 13:56:42 -0700

    added join to config/application

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,35 @@
 class RecipesController < ApplicationController
+
+	# displays up to 10 results at a time from search
+	def list
+	end
+
+	# Shows details about a recipe
+	def show
+	end
+
+	# makes api request using search term
+	def search
+	end
+
 end
+
+## FROM SLACKAPI
+# def index
+# 		@channels = SlackApiWrapper.list_channels
+# end
+#
+# def new
+# 	@channel = params[:channel]
+# end
+#
+# def create
+# 	channel = params[:channel]
+# 	message = params[:massage] # from form
+# 	if SlackApiWrapper.send_message(channel, message)
+# 		flash[:success] = "Message Sent!"
+# 	else
+# 		flash[:alert] = "Error Sending"
+# 	end
+# 	redirect_to chat_new_path(channel)
+# end

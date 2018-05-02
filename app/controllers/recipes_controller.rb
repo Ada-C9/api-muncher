a1d69fa41cac6028bb8ed9a0beb6f72cecfabe83
LetiTran <leticiastran@gmail.com>
2018-05-02 13:57:33 -0700

    Initialized rails files and defined routes and recipes controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,14 @@
+class RecipesController < ApplicationController
+
+  def root
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'recipes#root'
+  get ':recipe/new', to: 'recipes#new', as: 'recipe_new'
+  post ':recipe', to: 'recipes#create', as: 'recipe_create'
+
+end

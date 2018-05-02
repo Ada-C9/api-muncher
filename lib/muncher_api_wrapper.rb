2579ba5677b09232913a1a0b58a8e7e59d7c48b9
Tor Shimizu <torshimizu@gmail.com>
2018-05-02 11:21:59 -0700

    added file for muncher wrapper

---
+++ lib/muncher_api_wrapper.rb
@@ -0,0 +1,22 @@
+require 'httparty'
+require 'pry'
+
+module MuncherApiWrapper
+
+  def self.get_recipe(query)
+    app_id = ENV['MUNCHER_API_ID']
+    app_key = ENV['MUNCHER_API_KEY']
+
+    base_url = "https://api.edamam.com/search"
+    full_url = URI.encode(base_url + "?q=#{query}" "&app_id=#{app_id}&app_key=#{app_key}")
+
+    result = HTTParty.get(full_url)
+    binding.pry
+
+    unless result['ok']
+      raise StandardError.new('No Results')
+    else
+      return result['hits']
+    end
+  end
+end

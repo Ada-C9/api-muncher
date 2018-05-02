a70428d357da3ac49532398a1b010bbfb88fa814
Anne <annehwatson@gmail.com>
2018-05-02 12:17:07 -0700

    url initializes

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,18 +1,20 @@
 class Recipe
   #change this to be like http://eric-price.net/blog/rails-api-wrapper/
-  attr_reader :label
+  attr_reader :label, :url
 
-  def initialize(label)
+  def initialize(label, url)
     if label.nil? || label.empty?
       raise ArgumentError.new("The recipe needs a label")
     end
 
     @label = label
+    @url = url
   end
 
   def self.from_api(raw_recipe)
     self.new(
-      raw_recipe["recipe"]["label"]
+      raw_recipe["recipe"]["label"],
+      raw_recipe["recipe"]["url"]
     )
   end
 end

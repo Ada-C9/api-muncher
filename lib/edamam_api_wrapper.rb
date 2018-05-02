17c0a88ad8b2dff8fe42cdbf72c2861ad5e7fdf3
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-02 15:19:52 -0700

    Write API Wrapper list_recipes method

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -3,4 +3,10 @@ require 'httparty'
 class EdamamApiWrapper
   BASE_URL = "https://test-es.edamam.com/search"
 
+  def self.list_recipes(search_term)
+    url = BASE_URL + "?q=#{ search_term }"
+    data = HTTParty.get(url)
+    return data
+  end
+
 end # EdamamApiWrapper

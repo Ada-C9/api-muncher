7a05a8682c5391773b2a134eb573e30b20d00036
Phoebe <phoebe.manning8@gmail.com>
2018-05-02 15:48:12 -0700

    adds tests for root and list in recipe controller

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 describe RecipesController do
   let(:valid_search) { "cake" }
+  let(:invalid_search) { "?" }
 
   describe "root" do
     it "succeeds" do
@@ -11,13 +12,28 @@ describe RecipesController do
   end
 
   describe "list" do
-    it "succeeds with valid search text" do
+    it "returns an array of recipes with valid search text" do
       VCR.use_cassette("recipe") do
         search = valid_search
         response = RecipeSearchWrapper.list_recipes(search)
-        binding.pry
-        # response["ok"].must_equal true
-        # response["message"]["text"].must_equal message
+        response.must_be_kind_of Array
+        response.all? {|recipe| recipe.must_be_kind_of Recipe}
+      end
+    end
+
+    it "returns an empty array with invalid search" do
+      VCR.use_cassette("no_recipe") do
+        search = invalid_search
+        response = RecipeSearchWrapper.list_recipes(search)
+        response.must_equal []
+      end
+    end
+
+    it "returns an empty array with empty string" do
+      VCR.use_cassette("no_recipe") do
+        search = invalid_search
+        response = RecipeSearchWrapper.list_recipes(search)
+        response.must_equal []
       end
     end
   end

26a74e41812ea19ad2d9ab7adc0f9d9232cb57ad
Samantha Berk <sberk10@gmail.com>
2018-05-02 13:00:05 -0700

    Update api wrapper

--- lib/muncher_api_wrapper.rb
+++ lib/muncher_api_wrapper.rb
@@ -2,9 +2,18 @@ require 'httparty'
 require 'pry'
 
 class MuncherApiWrapper
+  BASE_URL = "https://api.edamam.com/"
+  APP_ID = ENV["EDAMAM_APPLICATION_ID"]
+  APP_KEY = ENV["EDAMAM_APPLICATION_KEY"]
 
-  def self.list_recipes
-
+  def self.search_recipes(search_term)
+    url = "#{BASE_URL}search?q=#{search_term}&app_id=#{APP_ID}&app_key=#{APP_KEY}"
+    data = HTTParty.get(url)
+    if data["recipes"]
+      return data["recipes"]
+    else
+      return []
+    end
   end
 
   def self.show_recipe

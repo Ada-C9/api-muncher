7e34d1b09a6c228ab82934b38672d1903a376a99
Kiera <kiera.thomasson@gmail.com>
2018-05-02 12:30:15 -0700

    created a recipes controller, added a api wrapper file. Stubbed out potential methods for wrapper class.

---
+++ lib/edamam_api_wrapper.rb
@@ -0,0 +1,20 @@
+require 'httparty'
+
+class EdamamApiWrapper
+  APP_ID = ENV['APP_ID']
+  APP_KEY= ENV['APP_KEY']
+  BASE_URL = "https://api.edamam.com/search"
+
+  def list_all_recipes()
+
+  end
+
+  def search_recipes()
+
+  end
+
+  def method_name
+
+  end
+
+end

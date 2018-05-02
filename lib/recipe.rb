bc52dd55bba4e4ba2a6ccf568fb8fc1b4d40e028
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-02 15:15:10 -0700

    Set up Recipe class

---
+++ lib/recipe.rb
@@ -0,0 +1,11 @@
+class Recipe
+
+  def initialize(name, photo_url, ingredients, dietary_labels, original_recipe_url)
+    @name = name
+    @photo_url = photo_url
+    @ingredients = ingredients
+    @dietary_labels = dietary_labels
+    @original_recipe_url = original_recipe_url
+  end
+
+end

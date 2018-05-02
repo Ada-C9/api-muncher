302e1a166f64edb708a6b86a0331da76cef30cac
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-05-02 14:57:11 -0700

    Updated Gemfile and te controller file along with all html pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,12 +5,10 @@ class RecipesController < ApplicationController
     @recipes = EdamamApiWrapper.list_recipes
   end
 
-  def new
-    @recipe_name = params[:recipe]
-  end
-
   def show
+    recipe_id = params[:id]
 
+    @recipe = Recipe.find(recipe_id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'recipes#index'
-  get ':recipe/new', to: 'recipes#new', as: 'new_recipes'
-  get ':recipe', to: 'recipes#show', as: 'show_recipes'
-  post ':recipe', to: 'recipe#create', as: 'send_recipe'
+
+  get '/recipes/:id', to: 'recipes#show', as: 'show_recipe'
 
 end

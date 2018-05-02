3e674fcb08af7d50993c81d3c31d9929842831f3
Tor Shimizu <torshimizu@gmail.com>
2018-05-02 15:22:20 -0700

    wrote test for get_one_recipe

--- test/lib/muncher_api_wrapper_test.rb
+++ test/lib/muncher_api_wrapper_test.rb
@@ -11,7 +11,20 @@ describe MuncherApiWrapper do
         end
       end
     end
-
-
   end # get_recipes
+
+  describe 'get_one_recipe' do
+    it 'must return a single recipe' do
+      VCR.use_cassette('recipes') do
+        recipe = MuncherApiWrapper.get_one_recipe(recipe_uri)
+
+        recipe.must_be_instance_of Recipe
+        recipe.name.must_equal recipe_name
+        recipe.uri.must_equal recipe_uri
+        recipe.ingredients.must_equal recipe_ingredients
+        recipe.image.must_equal recipe_image
+        recipe.dietary_info.must_equal dietary_info
+      end
+    end
+  end # get one recipe
 end # Muncher Api Wrapper

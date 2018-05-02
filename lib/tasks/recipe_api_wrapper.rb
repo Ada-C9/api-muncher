d30ff3ea794a83886ae2e4cc92903d98ae786865
Jamila <J.cornick21@gmail.com>
2018-05-02 16:56:19 -0700

    modified recipe wrapper

--- lib/tasks/recipe_api_wrapper.rb
+++ lib/tasks/recipe_api_wrapper.rb
@@ -1,36 +1,40 @@
-# require 'httparty'
-require 'pry'
+require 'httparty'
+# require 'pry'
 
-class SlackApiWrapper
-  class SlackError < StandardError; end
+class RecipeApiWrapper
+  class RecipeError < StandardError; end
 
-  def self.list_channels
-    token = ENV["SLACK_API_TOKEN"]
-    url = "https://slack.com/api/channels.list?token=#{token}"
+  def self.recipes(user_query)
+    token_id = ENV["wolverine"]
+    token_key = ENV["storm"]
+    BASE_URL =  https://api.edamam.com/search?q=
+
+    url = URI.encode(BASE_URL + "#{user_query}" + "&app_id" + token_id + "&app_key" + token_key)
+
     response = HTTParty.get(url)
 
     # ALWAYS CHECK YOUR ERROR CODES
     raise_on_error(response)
 
-    return response["channels"].map do |raw_channel|
-      Channel.from_api(raw_channel)
+    return response["hits"].map do |raw_recipes|
+      Recipe.from_api(raw_recipes)
     end
   end
 
-  def self.send_message(channel_name, message)
-    token = ENV["SLACK_API_TOKEN"]
-    url_root = "https://slack.com/api/chat.postMessage"
-    # Construct the full URL from the endpoint and the query params
-    full_url = URI.encode("#{url_root}?channel=#{channel_name}&text=#{message}&token=#{token}")
-
-    puts "About to send request to #{full_url}"
-
-    response = HTTParty.post(full_url)
-
-    raise_on_error(response)
-
-    # No return needed
-  end
+  # def self.send_message(channel_name, message)
+  #   token = ENV["SLACK_API_TOKEN"]
+  #   url_root = "https://slack.com/api/chat.postMessage"
+  #   # Construct the full URL from the endpoint and the query params
+  #   full_url = URI.encode("#{url_root}?channel=#{channel_name}&text=#{message}&token=#{token}")
+  #
+  #   puts "About to send request to #{full_url}"
+  #
+  #   response = HTTParty.post(full_url)
+  #
+  #   raise_on_error(response)
+  #
+  #   # No return needed
+  # end
 
   private
   def self.raise_on_error(response)

d56c31af5fc4ae4f8f87795b052e3d6d9496548e
Cara <cararbrennan@gmail.com>
2018-05-02 18:55:25 -0700

    Initial controller tests passing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,10 @@
 class RecipesController < ApplicationController
+
+  def index
+    #returns all recipes for GET request from API
+  end
+
+  def new
+#takes in a parameter for a new search and sends that to API 
+  end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -4,4 +4,22 @@ describe RecipesController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+
+  it "can get the index path" do
+  get root_path
+
+  must_respond_with :ok
+end
+
+it "can get the new recipe form" do
+  get new_search_path
+
+  must_respond_with :ok
+end
+
+# it "can send a message" do
+#   post chat_create_path("PLACEHOLDER")
+#
+#   must_redirect_to root_path
+# end
 end

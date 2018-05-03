90753b12ee5df354c96ec6304b358365ef223fee
Kaitlin Forsman <kcforsman@outlook.com>
2018-05-02 23:44:20 -0700

    added routes to routes.rb for the root, index, and show paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'recipe/root'
-  get 'recipe/index'
-  get 'recipe/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'recipe#root'
+
+  get 'recipes', to: 'recipe#index', as: 'recipes'
+  
+  get 'recipe/:recipe', to: 'recipe#show', as: 'recipe'
 end

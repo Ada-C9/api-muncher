36085c3171b19e96e0e41575ac55fff857187774
Kaitlin Forsman <kcforsman@outlook.com>
2018-05-02 17:26:16 -0700

    added necessary attributes to the recipe class and wrote tests that an instance can be initialized and has appropriate attr_reader methods

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,3 +1,13 @@
 class Recipe
-
+  attr_reader :id, :name, :photo, :url, :source, :ingredients, :diet_labels, :health_labels
+  def initialize(id, name, photo, url, source, ingredients, diet_labels, health_labels)
+    @id = id
+    @name = name
+    @photo = photo
+    @url = url
+    @source = source
+    @ingredients = ingredients
+    @diet_labels = diet_labels
+    @health_labels = health_labels
+  end
 end
--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -1,5 +1,59 @@
 require 'test_helper'
 
 describe Recipe do
+  it "can be initialized" do
+    recipe = Recipe.new(
+      "uri_id",
+      "Lemon Chicken",
+      "photo_url",
+      "original_url",
+      "Company",
+      [
+        "some ingredients",
+        "more ingredients",
+        "other ingredients"
+      ],
+      [
+        "diet thing"
+      ],
+      [
+        "health thing"
+      ]
+    )
 
+    recipe.must_be_instance_of Recipe
+  end
+
+  it "has an id, name, photo, recipe_url, source, ingredients array, dietary label array, and health label array" do
+    recipe = Recipe.new(
+      "uri_id",
+      "Lemon Chicken",
+      "photo_url",
+      "original_url",
+      "Company",
+      [
+        "some ingredients",
+        "more ingredients",
+        "other ingredients"
+      ],
+      [
+        "diet thing"
+      ],
+      [
+        "health thing"
+      ]
+    )
+
+    recipe.id.must_equal "uri_id"
+    recipe.name.must_equal"Lemon Chicken"
+    recipe.photo.must_equal "photo_url"
+    recipe.url.must_equal "original_url"
+    recipe.source.must_equal "Company"
+    recipe.ingredients.must_be_kind_of Array
+    recipe.ingredients.must_include "some ingredients"
+    recipe.diet_labels.must_be_kind_of Array
+    recipe.diet_labels.must_include "diet thing"
+    recipe.health_labels.must_be_kind_of Array
+    recipe.health_labels.must_include "health thing"
+  end
 end

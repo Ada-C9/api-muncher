e1e68a33cc8d0d94b3367a6b0027b97a9e71bbef
Nora Peters <norapeters5@gmail.com>
2018-05-02 22:15:13 -0700

    defines search#index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
+
+
   def new
   end
 
   def index
+    @search_terms = params[:search_terms]
+    @recipes = RecipeApiWrapper.find_recipes(params[:search_terms])
   end
 
   def show

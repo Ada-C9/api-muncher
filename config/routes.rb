a44b027077a45e35e738deb74cb0ae66ca2b034e
Kaitlin Forsman <kcforsman@outlook.com>
2018-05-03 00:27:33 -0700

    changed routes to fit the content of the params better, from recipe to uri

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'recipe#root'
 
   get 'recipes', to: 'recipe#index', as: 'recipes'
-  
-  get 'recipe/:recipe', to: 'recipe#show', as: 'recipe'
+
+  get 'recipe/:uri', to: 'recipe#show', as: 'recipe'
 end

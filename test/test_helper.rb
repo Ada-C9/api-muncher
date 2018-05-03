fd3788cc11899215661d04533301c64b176d5b5b
Jackie <jonofre16@hotmail.com>
2018-05-03 16:42:54 -0700

    Added VCR and created first test for EdamamApiWrapper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -35,9 +35,11 @@ VCR.configure do |config|
     :record => :new_episodes,    # record new data when we don't have it yet
     :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
   }
-  # Don't leave our Slack token lying around in a cassette file.
-  config.filter_sensitive_data("<SLACK_TOKEN>") do
-    ENV['SLACK_TOKEN']
+  config.filter_sensitive_data("<APP_ID>") do
+    ENV['EDAMAM_ID']
+  end
+  config.filter_sensitive_data("<APP_KEY>") do
+    ENV['EDAMAM_KEY']
   end
 end
 

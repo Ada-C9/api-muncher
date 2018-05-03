aedd2881362deac496a007a2777ceb969af6e106
Alexandria Brown <axbn2190@gmail.com>
2018-05-03 15:54:03 -0700

    rudimentary pagination

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,7 +19,9 @@ class RecipesController < ApplicationController
   end
 
   def index
-    @recipes = EdamamApiWrapper.list_recipes(params[:search])
+    @recipes = EdamamApiWrapper.list_recipes(params[:search]).paginate(:page => params[:page], :per_page => 10)
+
+
     # if @recipes.count < 1
     #   flash[:error] = "No recipes available for that search. Please try again!"
     # end

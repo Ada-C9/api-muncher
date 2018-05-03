0b53241f8c9063cef1597478a70651fdc993f6a8
Brittany Jones <brittany.r.jones@hotmail.com>
2018-05-03 10:16:20 -0700

    changed var names

--- lib/edamam-api-wrapper.rb
+++ lib/edamam-api-wrapper.rb
@@ -1,10 +1,18 @@
 require "HTTParty"
 
 class EdamamApiWrapper
-  BASE_URL = "https://api.edamam.com/search"
-  EDAMAM_ID = ENV["EDAMAM_ID"]
-  EDAMAM_KEY = ENV["EDAMAM_KEY"]
+  BASE_URL = "https://api.edamam.com/"
+  APP_ID = ENV["APP_ID"]
+  APP_KEY = ENV["APP_KEY"]
 
-  class ApiError < StandardError
+  def self.search(q)
+    response = HTTParty.get("#{BASE_URL}")
+  end
+
+
+  private
+  def self.create_recipe(api_params)
+    return Recipe.new(
+    )
   end
 end

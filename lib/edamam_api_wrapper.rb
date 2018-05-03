fed4fd9b1083e2afae46946072d7404525ec2b35
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-02 17:07:52 -0700

    started search form

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -6,7 +6,7 @@ class EdamamApiWrapper
   KEY = ENV["EDAMAM_API_KEY"]
 
   def self.list_recipes(query)
-    full_url = URI.encode("BASE_URL" + "?q=" + query + "&app_id=" + ID + "&app_key=" + KEY + "&from=0&to=50")
+    full_url = URI.encode(BASE_URL + "?q=" + query + "&app_id=" + ID + "&app_key=" + KEY + "&from=0&to=50")
 
     response = HTTParty.get(full_url).parsed_response
 

afaa20b7b6fd5b6727645b8c7d816c7b81019f6a
Nora Peters <norapeters5@gmail.com>
2018-05-02 22:25:27 -0700

    adds routes for root/new, index, and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'search/new'
-  get 'search/index'
-  get 'search/show'
+
+  root 'search#new'
+  get 'index', to: 'search#index', as: 'recipes'
+  get ':recipe/show', to: 'search#show', as: 'recipe_show'
+  #
+  # get 'search/new'
+  # get 'search/index'
+  # get 'search/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

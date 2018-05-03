0a797f80746e930ff0568ae6b4cc58431b0cf77c
Brittany Jones <brittany.r.jones@hotmail.com>
2018-05-03 14:11:07 -0700

    added initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  Rails.application.routes.draw do
+    root 'recipes#index'
+    get 'recipes/search', to: 'recipes#search', as: 'recipes_search'
+    get 'recipes/:id', to: 'recipes#show', as: 'recipe_show'
+  end

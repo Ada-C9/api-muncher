60b055f9350a18d2addfb4a927eb6cbb05be98e9
Samantha Berk <sberk10@gmail.com>
2018-05-03 16:44:19 -0700

    Add search bar and add create_recipe method to api_wrapper

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "recipes#search"
 
-  get "recipes#index"
+  # get "recipes#index"
 
-  get "recipes#show"
+  # get "recipes#show"
 end
--- lib/muncher_api_wrapper.rb
+++ lib/muncher_api_wrapper.rb
@@ -9,11 +9,24 @@ class MuncherApiWrapper
   def self.search_recipes(search_term)
     url = "#{BASE_URL}search?q=#{search_term}&app_id=#{APP_ID}&app_key=#{APP_KEY}"
     results = HTTParty.get(url)
+    recipes_list = []
     if results["hits"]
-      return results["hits"]
-    else
-      return []
+      results["hits"].each do |result_data|
+        recipes_list << create_recipe(result_data)
+      end
     end
+      return recipes_list
+  end
+
+  private
+  
+  def create_recipe(api_params)
+    return Recipe.new(
+      api_params["label"],
+      api_params["image"],
+      api_params["url"],
+      api_params["ingredientLines"]
+    )
   end
 
   def self.show_recipe

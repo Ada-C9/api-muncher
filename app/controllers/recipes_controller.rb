24321b3d735417fb7db97e00e07dd46f5eecfe46
0velez0 <mariaivelez@gmail.com>
2018-05-03 19:11:23 -0700

    create recipes controller, all views, routes, api wrapper, and recipe.rb file

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,16 @@
+class RecipesController < ApplicationController
+
+  def homepage
+    # this stays empty
+  end
+
+  def index
+    
+  end
+
+  def show
+
+  end
+
+
+end
---
+++ lib/edamam_api_wrapper.rb
@@ -0,0 +1,15 @@
+require 'httparty'
+
+class EdamamApiWrapper
+
+  BASE_URL = "https://api.edamam.com/search"
+  APP_ID = ENV['EDAMAM_ID']
+  APP_KEY = ENV['EDAMAM_KEY']
+
+  def self.search_one
+  end
+
+  def self.search_all
+  end
+
+end

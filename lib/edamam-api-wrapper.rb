5e19bd7b3f3f476c712b2651e1bff18fb44609b0
Brittany Jones <brittany.r.jones@hotmail.com>
2018-05-04 11:40:59 -0700

    added new implementation of api, added more views

--- lib/edamam-api-wrapper.rb
+++ lib/edamam-api-wrapper.rb
@@ -1,18 +1,36 @@
 require "HTTParty"
+require_dependency '../../lib/recipe'
 
 class EdamamApiWrapper
-  URL = "https://api.edamam.com/"
-  APP_ID = ENV["APP_ID"]
-  APP_KEY = ENV["APP_KEY"]
 
-  def self.search(q)
-    response = HTTParty.get("#{URL}" + "q=#{q}" + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEY}")
+  BASE_URL = "https://api.edamam.com/"
+  APP_KEY = ENV["EDAMAM_KEY"]
+  APP_ID = ENV["EDAMAM_ID"]
+
+  def self.search(query, app_key = nil, app_id = nil)
+    app_key ||= APP_KEY
+    app_id ||= APP_ID
+    query = URI.encode(query)
+    url = BASE_URL + "search?app_key=#{app_key}" + "&app_id=#{app_id}" + "&q=#{query}" + "&from=0&to=100"
+
+    data = HTTParty.get(url)
+
+        Recipe.new(
+          name: recipe_hash["recipe"]["label"],
+          id: recipe_hash["recipe"]["uri"].remove("http://www.edamam.com/ontologies/edamam.owl#recipe_"),
+          url: recipe_hash["recipe"]["url"],
+          ingredients: recipe_hash["recipe"]["ingredientLines"],
+          # optional values
+          dietary_info: recipe_hash["recipe"]["dietLabels"],
+          health_info: recipe_hash["recipe"]["healthLabels"],
+          image: recipe_hash["recipe"]["image"],
+          source: recipe_hash["recipe"]["source"],
+          yield:recipe_hash["recipe"]["yield"])
+      end
+      return recipes
+    else
+      return[]
+    end
   end
 
-
-  private
-  def self.create_recipe(api_params)
-    return Recipe.new(
-    )
-  end
 end

377b608e9f5630125a8ea520d7301fdbf482f41a
kseastman <kat.eastman@gmail.com>
2018-05-04 15:01:08 -0700

    css stuff.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -3,6 +3,9 @@ class Recipe
   attr_reader :label, :image, :orig_url, :ingredients, :diet, :health, :uri, :source
 
   def initialize(recipe_data)
+    # if label.nil? || label.empty?
+    #   raise ArgumentError.new("Need a label please")
+    # end
     @label = recipe_data[:label]
     @image = recipe_data[:image]
     @orig_url = recipe_data[:orig_url]
@@ -15,16 +18,15 @@ class Recipe
 
   def self.from_api(response)
     recipe_data = {
-    label: response["label"],
-    image: response["image"],
-    orig_url: response["url"],
-    ingredients: response["ingredientLines"],
-    source: response["source"],
-    diet: response["dietLabels"],
-    health: response["healthLabels"],
-    uri: response["uri"],
-  }
+      label: response["label"],
+      image: response["image"],
+      orig_url: response["url"],
+      ingredients: response["ingredientLines"],
+      source: response["source"],
+      diet: response["dietLabels"],
+      health: response["healthLabels"],
+      uri: response["uri"],
+    }
     self.new(recipe_data)
   end
-
 end

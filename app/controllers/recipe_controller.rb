f3a1a471608b597f4cfe808fa21c74acf5ff7271
Steffany Brown <steffannbrown@gmail.com>
2018-05-04 14:07:45 -0700

    added will_paginate gem

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -2,8 +2,8 @@ class RecipeController < ApplicationController
   def index
     query = params["query"]
 
-    @recipes = EdamamApiWrapper.find_it(query)
-
+    results = EdamamApiWrapper.find_it(query)
+    @recipes = results.paginate(:per_page => 2) 
     if @recipes.length > 0
       flash[:success] = "#{@recipes.length} Recipes Found!"
     else

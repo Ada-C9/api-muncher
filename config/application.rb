3479bae0575e26b65bf3d55ab3292b2d4a521e53
AngelaPoland <angelalpoland@gmail.com>
2018-05-04 14:25:39 -0700

    added a line of config to config application to try to get images on heroku

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module ApiMuncher
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.eager_load_paths << Rails.root.join('lib')
+    config.assets.initialize_on_precompile = false
   end
 end

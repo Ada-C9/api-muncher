1363eb5a85d03e768ce9dfc6594ffba9d9c73c2e
Brittany Jones <brittany.r.jones@hotmail.com>
2018-05-04 13:37:06 -0700

    initial testing for recipe controller

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 describe RecipesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe "search" do
+    it "can display the search form" do
+      get root_path
+      must_respond_with :success
+    end
+  end
+
+  describe "index" do
+    it "displays a list of recipes for a valid query" do
+    end
+
+    it "displays empty index if the queryt yields no results" do
+    end
+  end
+
+  describe "show" do
+    it "can display the show page for a valid recipe from the query" do
+    end
+
+    it "redirects to a 404 page if the recipe uri is invalid" do
+    end
+  end
 end

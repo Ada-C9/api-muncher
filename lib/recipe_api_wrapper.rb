18ee64c1892a402cd08f23bbe46f4ffd69aa73bd
Nora Peters <norapeters5@gmail.com>
2018-05-04 12:19:21 -0700

    empties recipes_list in apiwrapper each time find_recipes is called. adds get_recipe method for search#show

--- lib/recipe_api_wrapper.rb
+++ lib/recipe_api_wrapper.rb
@@ -6,19 +6,26 @@ class RecipeApiWrapper
   ID = ENV["APP_ID"]
   KEY = ENV["APP_KEY"]
 
+  @recipes_list = []
+
   def self.find_recipes(search_terms)
+
+    if !@recipes_list.empty?
+      @recipes_list = []
+    end
+
     encoded_search_terms = URI.encode(search_terms)
-    response = HTTParty.get("#{URL}?q=#{encoded_search_terms}&app_id=#{ID}&app_key=#{KEY}")
-    recipes_list = []
+    response = HTTParty.get("#{URL}?q=#{encoded_search_terms}&app_id=#{ID}&app_key=#{KEY}&to=60")
     if response["hits"]
       response["hits"].each do |hit|
-        recipes_list << Recipe.new(hit["recipe"]["label"], hit["recipe"]["image"])
+        @recipes_list << Recipe.new(hit["recipe"])
       end
     end
-    return recipes_list
+    return @recipes_list
   end
 
-  def self.get_recipe
+  def self.get_recipe(id)
+    @recipes_list.find { |recipe| recipe.id == id }
   end
 
 end

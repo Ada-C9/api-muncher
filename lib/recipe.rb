918d68777b1150859d6fb37f5203703d15234ca7
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-05-04 12:17:59 -0700

    test for recipes

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -10,5 +10,7 @@ class Recipe
     @ingredient_lines = params[:ingredient_lines]
     @diet_labels = params[:diet_labels]
     @health_labels = params[:health_labels]
+
+    raise ArgumentError if @label.nil? || @label.empty? || @uri.nil? || @uri.empty?
   end
 end

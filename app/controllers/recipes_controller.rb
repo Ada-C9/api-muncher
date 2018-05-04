13c66c13903764bd1dccb3825aea1c3fdc83ed6d
knockknockhusthere <angela.huxia@gmail.com>
2018-05-03 20:59:49 -0700

    testing output for pesto chicken in index views page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   def index
     @recipes = RecipeApiWrapper.list_recipes
-    @recipe = RecipeApiWrapper.show_recipe("http://www.edamam.com/ontologies/edamam.owl#recipe_48c52ac3bd8c16720a7cb8ef34cd5e5b")
+    # @recipe = RecipeApiWrapper.show_recipe("http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_48c52ac3bd8c16720a7cb8ef34cd5e5b")
   end
 
   def show
--- lib/recipe_api_wrapper.rb
+++ lib/recipe_api_wrapper.rb
@@ -7,24 +7,29 @@ class RecipeApiWrapper
   API_KEY = ENV["EDAMAM_API_KEY"]
   API_ID = ENV["EDAMAM_API_ID"]
 
+  search_results = []
   def self.list_recipes
 
     query = "chicken pesto"
     url = "https://api.edamam.com/search?app_id=#{API_ID}&app_key=#{API_KEY}&q=#{query}"
 
-    response = HTTParty.get(url)
+    encoded_uri = URI.encoded(url)
+
+    response = HTTParty.get(url).parsed_response
     #ALWAYS CHECK YOUR ERROR CODES
     unless response.success?
       raise StandardError.new(response["error"])
     end
 
-    return response["hits"].map do |raw_recipe|
+    return response["hits"].each do |raw_recipe|
       Recipe.from_api(raw_recipe["recipe"])
     end
   end
 
-  def self.show_recipe(uri)
+  def show_recipe(uri)
     url = "https://api.edamam.com/search?app_id=#{API_ID}&app_key=#{API_KEY}&r=#{uri}"
+
+    encoded_uri = URI.encoded(url)
 
     response = HTTParty.get(url)
     #ALWAYS CHECK YOUR ERROR CODES
@@ -32,7 +37,7 @@ class RecipeApiWrapper
       raise StandardError.new(response["error"])
     end
 
-    Recipe.from_api(response[0])
+    return Recipe.from_api(response[0])
   end
 
   private

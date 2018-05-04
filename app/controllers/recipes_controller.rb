a1e172b37122780720d1df6ec435da66a3c49d78
Samantha Berk <sberk10@gmail.com>
2018-05-03 19:10:38 -0700

    Search term persists and shows recipe title

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,17 @@
 class RecipesController < ApplicationController
+  def root
+  end
+
   def search
     recipes = MuncherApiWrapper.search_recipes(params[:search_term])
+    redirect_to index_path(search_term: params[:search_term])
+
   end
 
   def index
+    search_term = params[:search_term]
+    @recipes = MuncherApiWrapper.search_recipes(search_term)
+    # raise
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "recipes#search"
+  root "recipes#root"
   post "search" => "recipes#search", as: :search
 
 
-  # get "recipes#index"
+  get "index" => "recipes#index", as: :index
 
   # get "recipes#show"
 end

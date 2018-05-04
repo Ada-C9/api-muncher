d01ffacb5c8c8aa523e52019a1ae53862f8a0f04
LetiTran <leticiastran@gmail.com>
2018-05-04 15:21:01 -0700

    Completed tests on Recipe class for now.

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module ApiMuncher
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
-    
+
     config.generators do |g|
       # Force new test files to be generated in the minitest-spec style
       g.test_framework :minitest, spec: true

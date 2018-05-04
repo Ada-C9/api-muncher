c6fbb6586c4b1fe6ceab3d7a30afcbc6bb22d5db
LetiTran <leticiastran@gmail.com>
2018-05-04 16:11:14 -0700

    Started tests for api

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -1,1 +1,16 @@
 require 'test_helper'
+
+describe EdamamApiWrapper do
+
+  it "Can receive valid data from Edamam for a recipe" do
+  VCR.use_cassette("recipes") do
+    response = EdamamApiWrapper.find_recipes("chicken")
+
+    response.must_be_kind_of Array
+    assert response.any?
+
+  end
+  end
+
+
+end

21d5fadd3f9f6a4457774d7e07a3a7c77fc0f42c
LetiTran <leticiastran@gmail.com>
2018-05-04 15:20:36 -0700

    Changed argument passed to Recipe a hash as an argument

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -11,20 +11,23 @@ class EdamamApiWrapper
     @recipe_list.clear
 
     response = HTTParty.get("#{URL}?q=#{query}&app_id=#{ID}&app_key=#{KEY}&to=50")
-# raise
+    # raise
 
     if response["hits"] && response["count"] > 0
       response.parsed_response["hits"].each do |hit|
-        @recipe_list << Recipe.new(
-          hit["recipe"]["label"],
-          hit["recipe"]["uri"].match(/[^_]*$/)[0],
-          hit["recipe"]["image"],
-          hit["recipe"]["source"],
-          hit["recipe"]["url"],
-          hit["recipe"]["yield"],
-          hit["recipe"]["ingredientLines"],
-          hit["recipe"]["dietLabels"])
-        end
+        info_hash = {
+          title: hit["recipe"]["label"],
+          id: hit["recipe"]["uri"].match(/[^_]*$/)[0],
+          image: hit["recipe"]["image"],
+          source: hit["recipe"]["source"],
+          url: hit["recipe"]["url"],
+          portion: hit["recipe"]["yield"],
+          ingredients: hit["recipe"]["ingredientLines"],
+          diet: hit["recipe"]["dietLabels"]
+        }
+
+        @recipe_list << Recipe.new(info_hash)
+      end
     end
     return @recipe_list
   end

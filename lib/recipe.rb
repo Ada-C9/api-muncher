3938ca9a7ede70dc40a78897cb4a63c5de2608b0
Nora Peters <norapeters5@gmail.com>
2018-05-04 12:18:33 -0700

    refactors recipe class in lib

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,10 +1,13 @@
 class Recipe
+  attr_reader :name, :id, :image, :contributor, :ingredients, :dietary_labels, :health_labels
 
-  attr_reader :label, :image
-
-  def initialize(label, image, options = { })
-    @label = label
-    @image = image
+  def initialize(recipe)
+    @id = recipe["uri"].partition('_').last
+    @name = recipe["label"]
+    @image = recipe["image"]
+    @contributor = recipe["source"]
+    @ingredients = recipe["ingredientLines"].split(",").flatten
+    @dietary_labels = recipe["dietLabels"]
+    @health_labels = recipe["healthLabels"]
   end
-
 end

b293b1d945e1dc8c46ccbfa70f1d72938553509d
Nora Peters <norapeters5@gmail.com>
2018-05-04 12:10:26 -0700

    adds search#show

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,20 @@
 class SearchController < ApplicationController
 
-
   def new
   end
 
   def index
     @search_terms = params[:search_terms]
-    @recipes = RecipeApiWrapper.find_recipes(params[:search_terms])
+    @recipes = RecipeApiWrapper.find_recipes(@search_terms)
   end
 
   def show
+    id = params[:id]
+    @recipe = RecipeApiWrapper.get_recipe(id)
+    if @recipe.nil?
+      flash[:alert] = "Recipe does not exist"
+      redirect_back(fallback_location: root_path)
+    end
   end
+
 end

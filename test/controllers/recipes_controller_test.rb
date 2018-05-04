42b6a4dee68085eb2828a064213932c74b2b8f42
Wenjie <traxiaojie@gmail.com>
2018-05-04 13:48:23 -0700

    Remove showing diet labels if there is none

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -3,12 +3,14 @@ require "test_helper"
 describe RecipesController do
   describe "index" do
     it "can show a list of recipes with desired flavor" do
-      get recipes_path, params: {flavor: "tofu"}
 
-      must.response_with :success
+      flavor = "tofu"
+      get recipes_path(flavor)
+
+      must_respond_with :success
     end
 
-    
+
 
   end
 end

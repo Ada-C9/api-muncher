c1a7fbace448bd159686a1ab8a68bff69c31bc83
Steffany Brown <steffannbrown@gmail.com>
2018-05-04 14:22:34 -0700

    updated pagination code in recipe controller and index view

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -3,7 +3,7 @@ class RecipeController < ApplicationController
     query = params["query"]
 
     results = EdamamApiWrapper.find_it(query)
-    @recipes = results.paginate(:per_page => 2) 
+    @recipes = results.paginate(:page => params[:page], :per_page => 2) 
     if @recipes.length > 0
       flash[:success] = "#{@recipes.length} Recipes Found!"
     else

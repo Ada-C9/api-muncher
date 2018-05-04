65f355b8f75cd47f94eb90cc3a238f3bf72929d3
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-05-04 11:21:16 -0700

    added grid and flexbox to show and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "recipes#homepage"
+
   get "homepage", to: "recipes#homepage"
   get "/recipes", to: "recipes#index", as: "recipes"
   get "/recipes/:id", to:"recipes#show", as: "recipe"

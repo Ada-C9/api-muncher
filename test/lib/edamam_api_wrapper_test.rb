f9ed570f3f42ae24e61ecca25549dbf779ace757
Aruna <aruna.thungathurthi@gmail.com>
2018-05-05 15:18:03 -0700

    added wrapper tests

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -3,29 +3,24 @@ require 'test_helper'
 describe EdamamApiWrapper do
   describe'list_recipes' do
 
-    it"returns list of 10 recipes if given a search term" do
+    it "can return an array of recipe objects" do
+      VCR.use_cassette("search") do
+        recipes = EdamamApiWrapper.list_recipes("chocolate")
 
-      VCR.use_cassette("recipes") do
+        recipes.must_be_instance_of Array
 
-        data = EdamamApiWrapper.list_recipes("fish")
-        data.count.must_equal 10
-        data.must_be_instance_of Array
-
+        recipes.each do |r|
+          r.must_be_instance_of Recipe
+        end
       end
     end
-
-    it"returns empty array if given a empty string assearch term" do
-
-      VCR.use_cassette("recipes") do
-
-        data = EdamamApiWrapper.list_recipes("")
-        data.count.must_equal 0
-        data.must_equal []
-
-      end
-    end
-
   end
 
-  
+  it "will return an empty array for a search that returns no results" do
+      VCR.use_cassette("search") do
+        recipes = EdamamApiWrapper.list_recipes("")
+
+        recipes.must_equal []
+      end
+    end
 end

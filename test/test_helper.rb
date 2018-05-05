cc6fff6975c0206f3d9332e80838700aff384d5a
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-05 14:23:53 -0700

    Set up test configuration and gems

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,27 @@ Minitest::Reporters.use!(
   Minitest.backtrace_filter
 )
 
+VCR.configure do |config|
+ config.cassette_library_dir = 'test/cassettes' # folder where casettes will be located
+ config.hook_into :webmock # tie into this other tool called webmock
+ config.default_cassette_options = {
+   :record => :new_episodes,    # record new data when we don't have it yet
+   :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
+ }
+ # Don't leave our Edamam token lying around in a cassette file.
+ config.filter_sensitive_data("<EDAMAM_APP_ID>") do
+   ENV['EDAMAM_APP_ID']
+ end
+
+ config.filter_sensitive_data("<EDAMAM_APP_KEY>") do
+   ENV['EDAMAM_APP_KEY']
+ end
+end
+
+if ActionPack::VERSION::STRING >= "5.2.0"
+  Minitest::Rails::TestUnit = Rails::TestUnit
+end
+
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:

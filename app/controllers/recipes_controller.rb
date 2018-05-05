8b6554d20b56d316ed9dd02d2b442cbbd3283128
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-05-05 13:35:39 -0700

    Tried different approaches to displaying error messages for failed API calls

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,31 @@
 require 'will_paginate/array'
 
 class RecipesController < ApplicationController
+
+  around_action :catch_api_error
+
   def search; end
 
   def index
     query = params[:query]
+
     @recipes = EdamamApiWrapper.search_results(query).paginate(page: params[:page], per_page: 12)
   end
 
   def show
     id = params[:id]
+
     @recipe = EdamamApiWrapper.recipe_details(id)
+  end
+
+  private
+  def catch_api_error
+    begin
+      yield
+    rescue Standard_Error
+      flash[:status] = :failure
+      flash[:message] = "API called failed"
+      redirect_back fallback_location: root_path
+    end
   end
 end
--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -4,12 +4,12 @@ class EdamamApiWrapper
   def self.search_results(query)
     app_id = ENV["EDAMAM_APP_ID"]
     app_key = ENV["EDAMAM_APP_KEY"]
-    url = "https://api.edamam.com/search?q=#{query}&app_id=#{app_id}&app_key=#{app_key}&from=0&to=24"
+    url = "https://api.edamam.com/search?q=#{query}&app_id=#{app_id}&app_key=#{app_key}&from=0&to=120"
 
     response = HTTParty.get(url).parsed_response
 
     unless response["count"] > 0
-      raise StandardError.new(response["error"])
+      raise StandardError.new
     end
 
     return response["hits"].map do |raw_recipe|
@@ -26,7 +26,7 @@ class EdamamApiWrapper
     response = HTTParty.get(url).parsed_response
 
     unless response.count > 0
-      raise StandardError.new(response["error"])
+      raise StandardError.new
     end
 
     return  Recipe.from_recipe_api(response)

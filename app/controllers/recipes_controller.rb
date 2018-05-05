c0bc0a3796fdccb1ec80572c5bee0c10869f146d
Brenda Rios <riosbrenda@gmail.com>
2018-05-04 22:56:49 -0700

    Added flash message in controller and in the views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,19 +4,26 @@ class RecipesController < ApplicationController
 
   def home
   end
-  
+
+
   def index
+    @search_term = params[:search_term]
 
+    if @search_term.blank?
 
-    if params[:search_term]
+      flash[:status] = :failure
+      flash[:message] = "Please enter a search term"
+      redirect_to root_path
+      return result_recipes = nil
+
+    else
       result_recipes = EdamamApiWrapper.list_of_recipes(params[:search_term])
-
-      # added Kaminari gem to make the pagination
-      @recipes = Kaminari.paginate_array(result_recipes).page(params[:page]).per(10)
-
     end
-  end
+    # added Kaminari gem to make the pagination
+    @recipes = Kaminari.paginate_array(result_recipes).page(params[:page]).per(10)
 
+
+  end
 
 
   def show

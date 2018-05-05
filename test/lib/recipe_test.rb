f2f9c80479a63403ff96399ac4029f880159775d
Brenda Rios <riosbrenda@gmail.com>
2018-05-05 12:16:55 -0700

    Tests for class recipe.rb passing

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -4,6 +4,23 @@ describe Recipe do
 
   before do
 
+    @uri = "http://www.edamam.com/ontologies/edamam.owl#recipe_b79327d05b8e5b838ad6cfd9576b30b6"
+    @label = "Chicken Vesuvio"
+    @url = "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html"
+    @dietLabels = ["Low-Carb"]
+    @ingredientLines = [
+      "1/2 cup olive oil",
+      "5 cloves garlic, peeled",
+      "2 large russet potatoes, peeled and cut into chunks",
+      "1 3-4 pound chicken, cut into 8 pieces (or 3 pound chicken legs)",
+      "3/4 cup white wine",
+      "3/4 cup chicken stock",
+      "3 tablespoons chopped parsley",
+      "1 tablespoon dried oregano",
+      "Salt and pepper",
+      "1 cup frozen peas, thawed"]
+    @image = "https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg"
+    @source = "Serious Eats"
   end
 
   it "Must initialize with the required parameters (uri, label, url) and optional variables ( image, source, ingredientLines, dietLabels)" do
@@ -27,6 +44,12 @@ describe Recipe do
     proc {
       Recipe.new(@uri, @label)
     }.must_raise ArgumentError
+
+    proc {
+      Recipe.new(@uri, @url)
+    }.must_raise ArgumentError
+
+
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,7 +21,7 @@ VCR.configure do |config|
     :record => :new_episodes,    # record new data when we don't have it yet
     :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
   }
-  # Don't leave our Slack token lying around in a cassette file.
+  # Don't leave our Edamam token lying around in a cassette file.
   config.filter_sensitive_data("<EDAMAM_ID>") do
     ENV['EDAMAM_ID']
   end

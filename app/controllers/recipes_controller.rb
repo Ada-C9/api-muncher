7ebb57d78da8b2375a737bd0055d1f6402c2e75a
Brenda Rios <riosbrenda@gmail.com>
2018-05-04 18:44:13 -0700

    Refactored controllers.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,29 @@
+require 'kaminari'
+
 class RecipesController < ApplicationController
+
+  def home
+  end
+  def index
+
+
+    if params[:search_term]
+      result_recipes = EdamamApiWrapper.list_of_recipes(params[:search_term])
+
+      # added Kaminari gem to make the pagination
+      @recipes = Kaminari.paginate_array(result_recipes).page(params[:page]).per(12)
+
+    end
+  end
+
+
+
+  def show
+    @recipe = EdamamApiWrapper.show_recipe(params[:uri])
+
+    if !@recipe
+      return render_404
+    end
+  end
+
 end

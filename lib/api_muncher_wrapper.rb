71cf062de6f1b29d1394313a285ac52e3ef035ab
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-05-05 16:17:28 -0700

    change method in wrapper to not rely on class variable

--- lib/api_muncher_wrapper.rb
+++ lib/api_muncher_wrapper.rb
@@ -4,7 +4,7 @@ class ApiMuncherWrapper
   APP_KEY = ENV["APP_KEY"]
 
 
-  @recipe_list = []
+  # @recipe_list = []
 
   def self.search_recipes(search)
 
@@ -12,10 +12,10 @@ class ApiMuncherWrapper
     data = HTTParty.get(url)
 
     if data["hits"]
-      @recipe_list = []
+      recipe_list = []
       data["hits"].each do |hash|
         recipe = hash["recipe"]
-        @recipe_list << Recipe.new(recipe["label"],
+        recipe_list << Recipe.new(recipe["label"],
           recipe["uri"], {image: recipe ["image"],
             source: recipe['source'],
             url: recipe['url'],
@@ -23,18 +23,36 @@ class ApiMuncherWrapper
             dietary_info:recipe['dietLabels']}
           )
       end
-      return @recipe_list
+      return recipe_list
     end
 
   end
 
   def self.find_recipe(id)
-    @recipe_list.each do |recipe|
-      if recipe.id == id
-        return recipe
-      end
-    end
-    return nil
-  end
+
+  #   @recipe_list.each do |recipe|
+  #     if recipe.id == id
+  #       return recipe
+  #     end
+  #   end
+  #   return nil
+  # end
+
+  url = BASE_URL +
+   "?r=http://www.edamam.com/ontologies/edamam.owl%23recipe_#{id}&app_id=#{APP_ID}&app_key=#{APP_KEY}"
+   recipe = HTTParty.get(url).parsed_response[0]
+   if recipe.nil?
+     return nil
+   else
+     return Recipe.new(recipe["label"],
+     recipe["uri"],
+     image: recipe["image"],
+     source: recipe["source"],
+     url: recipe["url"],
+     ingredients: recipe["ingredientLines"],
+     dietary_info: recipe["dietLabels"])
+   end
+ end
+
 
 end

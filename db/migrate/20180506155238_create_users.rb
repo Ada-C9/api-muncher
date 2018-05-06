5528394f379f0f6dde2278b48a5e19310cfc7a2a
Tor Shimizu <torshimizu@gmail.com>
2018-05-06 08:55:01 -0700

    created a user model to track users

---
+++ db/migrate/20180506155238_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :uid, null: false
+      t.string :provider, null: false
+      t.timestamps
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -46,4 +46,8 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+
+  def mock_hash(user)
+
+  end
 end

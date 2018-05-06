716beea8e67c6daed42cf9c2bba505d37a87a6f4
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-05 17:35:15 -0700

    Test Recipes controller 'index' action

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 describe RecipesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe 'index' do
+    it 'can get the index path' do
+      get root_path
+      must_respond_with :ok
+    end # it do
+
+    it 'can get recipes from a search' do
+      VCR.use_cassette("recipes") do
+        get recipes_path, params: { search: 'naseu' }
+        must_respond_with :ok
+      end
+    end # it do
+  end # index
+
+  describe 'show' do
+  end
+
 end # RecipesController

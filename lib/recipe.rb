d1e0966e00b3a4b37c61cf1587e2b50aa917c9d2
Leticia Tran <leticiastran@gmail.com>
2018-05-05 21:05:45 -0700

    Addapted to only initialize details of a recipe if they are given by api_wrapper (only happens if using #find method)

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -8,11 +8,14 @@ class Recipe
     @title = info_hash[:title]
     @id = info_hash[:id]
     @image = info_hash[:image]
-    @source = info_hash[:source]
-    @url = info_hash[:url]
-    @portion = info_hash[:portion]
-    @ingredients = info_hash[:ingredients]
-    @diet = info_hash[:diet]
+
+    if info_hash.length > 3
+      @source = info_hash[:source]
+      @url = info_hash[:url]
+      @portion = info_hash[:portion]
+      @ingredients = info_hash[:ingredients]
+      @diet = info_hash[:diet]
+    end
   end
 
 end

2298fa071cd27e5b545f67ec5009b6003ad7e301
Tor Shimizu <torshimizu@gmail.com>
2018-05-06 13:00:33 -0700

    added a favorites model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,14 @@
 class RecipesController < ApplicationController
 
+  before_action :save_recent_searches
+
   def root
   end
 
   def index
     @query = params[:query]
     @recipes = MuncherApiWrapper.get_recipes(@query).paginate(:page => params[:page], :per_page => 12)
+    session[:recent_searches] << @query
   end
 
   def show
@@ -13,4 +16,8 @@ class RecipesController < ApplicationController
     @recipe = MuncherApiWrapper.get_one_recipe(recipe_uri)
   end
 
+  private
+  def save_recent_searches
+    session[:recent_searches] = []
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_06_155238) do
+ActiveRecord::Schema.define(version: 2018_05_06_194850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "name"
+    t.string "uri"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

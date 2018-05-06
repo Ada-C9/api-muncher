989b51d7ce16a5a0f611231e21d8d437afe3f672
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-05-05 21:37:35 -0700

    Add controller tests

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -2,18 +2,41 @@ require "test_helper"
 
 describe RecipesController do
   it "should get index" do
-    get recipes_index_url
-    value(response).must_be :success?
+    VCR.use_cassette("recipes") do
+      get recipes_path(q: "chicken")
+      must_respond_with :success
+    end
   end
 
-  it "should get show" do
-    get recipes_show_url
-    value(response).must_be :success?
-  end
+    it "must get results page when there are no results" do
+      VCR.use_cassette("recipes") do
+        get recipes_path(q: "")
+        must_respond_with :success
+      end
+    end
 
-  it "should get search" do
-    get recipes_search_url
-    value(response).must_be :success?
-  end
+    it "should get show" do
+      VCR.use_cassette("recipes") do
+        recipe_uri = "http://www.edamam.com/ontologies/edamam.owl%23recipe_9b5945e03f05acbf9d69625138385408"
+        get recipe_path(uri: recipe_uri)
 
-end
+        must_respond_with :success
+      end
+    end
+
+    it "renders 404 if the recipe is not found" do
+      VCR.use_cassette("recipes") do
+        fake_uri = "http://www.edamam.com/ontologies/edamam.owl%23recipe_fake-uri"
+        get recipe_path(uri: fake_uri)
+
+        must_respond_with :missing
+      end
+    end
+
+
+    it "should get search" do
+      get root_path
+      must_respond_with :success
+    end
+    
+  end

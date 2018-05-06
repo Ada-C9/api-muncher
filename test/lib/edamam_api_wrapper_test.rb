0af701f04e512ea82e453e2373b08cb4104cebc7
kseastman <kat.eastman@gmail.com>
2018-05-06 01:11:53 -0700

    rewrote lost tests.

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 describe EdamamApiWrapper do
   describe 'search' do
-    it "gives a list of recipes" do
+    it "calls keyword_search to give a list of recipes" do
       VCR.use_cassette('chicken') do
         recipes = EdamamApiWrapper.search("chicken")
 
@@ -12,8 +12,33 @@ describe EdamamApiWrapper do
       end
     end
 
-    it "text" do
-
+    it "raises an error if an invalid search is returned" do
+      VCR.use_cassette('chicken') do
+        proc { EdamamApiWrapper.search("Europa")}.must_raise
+      end
     end
+
+    it "calls recipe_search to return a single recipe" do
+      VCR.use_cassette('chicken') do
+        recipes = EdamamApiWrapper.search("chicken")
+
+        uri = recipes.first.uri.split('owl')[1]
+
+        recipe = EdamamApiWrapper.search(uri)
+        result = recipe.uri.include?(uri)
+
+        recipe.must_be_kind_of Recipe
+        result.must_equal true
+
+      end
+    end
+
+    it "raises an error if an invalid search result occured" do
+      VCR.use_cassette('chicken') do
+        uri = "#recipe_slider"
+        proc {EdamamApiWrapper.search(uri)}.must_raise
+      end
+    end
+    
   end
 end

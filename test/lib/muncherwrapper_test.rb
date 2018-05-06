da1b0a9126b45034d81aeb03e0e40301550981b5
AngelaPoland <angelalpoland@gmail.com>
2018-05-06 11:49:05 -0700

    added config.filter_sensitive_data info into test helper

--- test/lib/muncherwrapper_test.rb
+++ test/lib/muncherwrapper_test.rb
@@ -2,8 +2,28 @@ require 'test_helper'
 
 describe MuncherWrapper do
 
+  describe "self.get_recipes(search_phrase)" do
 
+    it "can get list of search results" do
 
+    end
+
+    it "will return an empty array with a bad search" do
+
+    end
+  end
+
+  describe "self.show_recipe(uri)" do
+
+    it "will show one recipe" do
+
+    end
+
+    it "will return nil if uri is blank or response does not exist" do
+
+    end
+
+  end
 
 
 end

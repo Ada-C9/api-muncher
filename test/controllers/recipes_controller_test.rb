19711077b0053dcf05d1262f8cfe82574b8cc614
Aruna <aruna.thungathurthi@gmail.com>
2018-05-05 23:47:06 -0700

    recipe controller tests

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,14 +1,34 @@
 require "test_helper"
 
+require "test_helper"
+
 describe RecipesController do
-  it "should get index" do
-    get recipes_index_url
-    value(response).must_be :success?
+  describe 'home' do
+    it "succeeds" do
+      get root_path
+
+      must_respond_with :success
+    end
+  end
+
+  describe 'index' do
+    it "succeeds when valid query is provided only" do
+      VCR.use_cassette("recipes") do
+        get root_path, params: {
+          query: "tofu"
+        }
+
+        must_respond_with :success
+      end
+    end
   end
 
   it "should get show" do
-    get recipes_show_url
-    value(response).must_be :success?
-  end
+    VCR.use_cassette("recipes") do
+      recipe_uri = "http://www.edamam.com/ontologies/edamam.owl%23recipe_9b5945e03f05acbf9d69625138385408"
+      get recipe_path(uri: recipe_uri)
 
+      must_respond_with :success
+    end
+  end
 end

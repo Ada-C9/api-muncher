254bdf033639ed86e70afbfe72ef8767c62263ce
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-05-05 23:29:50 -0700

    all basic core functionality is here. Still needs CSS, heroku, and tests, but this is a solid start.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
   root 'recipes#search'
 
-  resources :recipes, only: [:index]
-
   get 'recipes/search', to: 'recipes#search', as: 'recipes_search'
   get 'recipes/detail', to: 'recipes#detail', as: 'recipes_detail'
 
--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -8,11 +8,8 @@ class EdamamApiWrapper
   R_SEARCH_DRESSING = "http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_" # This is the encoded https into that Edamam wants as a leader for the recipe's ID number for doing an 'r' search.
 
   def self.recipe_search_result(query)
-
     list_response = HTTParty.get("#{SEARCH_URL}q=#{query}&app_id=#{APP_ID}&app_key=#{APP_KEY}&from=0&to=40")
-
     recipe_list = []
-
     if list_response["hits"]
       list_response["hits"].each do |hit|
         @recipe =  Recipe.new(hit["recipe"]["label"], hit["recipe"]["image"], hit["recipe"]["uri"], hit["recipe"]["source"], hit["recipe"]["url"], hit["recipe"]["ingredientLines"], hit["recipe"]["dietLabels"])
@@ -21,19 +18,18 @@ class EdamamApiWrapper
     else
       return []
     end
-
     return recipe_list
-
   end
 
   def self.specific_recipe(target_uri)
-
     form_encoded_recipe_URI = CGI::escape(target_uri)
     recipe_response = HTTParty.get("#{SEARCH_URL}app_id=#{APP_ID}&app_key=#{APP_KEY}&r=#{form_encoded_recipe_URI}")
-
     if !recipe_response.empty?
-      @target_recipe = Recipe.new(hit["recipe"]["label"], hit["recipe"]["image"], hit["recipe"]["uri"], hit["recipe"]["source"], hit["recipe"]["ingredientLines"], hit["recipe"]["dietLabels"] )
+      target_recipe = Recipe.new(recipe_response[0]["label"], recipe_response[0]["image"], recipe_response[0]["uri"], recipe_response[0]["source"], recipe_response[0]["url"], recipe_response[0]["ingredientLines"], recipe_response[0]["dietLabels"])
+    else
+      target_recipe = nil
     end
+    return target_recipe
   end
 
 end

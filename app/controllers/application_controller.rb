e59706749244b4a112c57c667a0aa6f8713c84e3
Tor Shimizu <torshimizu@gmail.com>
2018-05-06 13:08:44 -0700

    added a recent searches list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :find_user
+  before_action :save_recent_searches
 
   private
 
@@ -7,5 +8,10 @@ class ApplicationController < ActionController::Base
     if session[:user_id]
       @logged_user = User.find_by(id: session[:user_id])
     end
+  end
+
+  private
+  def save_recent_searches
+    session[:recent_searches] = [] if session[:recent_searches].nil?
   end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,23 +1,16 @@
 class RecipesController < ApplicationController
 
-  before_action :save_recent_searches
-
   def root
   end
 
   def index
     @query = params[:query]
     @recipes = MuncherApiWrapper.get_recipes(@query).paginate(:page => params[:page], :per_page => 12)
-    session[:recent_searches] << @query
+    session[:recent_searches] << @query unless session[:recent_searches].include?(@query)
   end
 
   def show
     recipe_uri = params[:uri]
     @recipe = MuncherApiWrapper.get_one_recipe(recipe_uri)
-  end
-
-  private
-  def save_recent_searches
-    session[:recent_searches] = []
   end
 end

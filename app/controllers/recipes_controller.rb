d00281609c89503806e6ad0ebafd317b193e5fc2
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-06 09:39:36 -0700

    moved form for search, added new route and method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,17 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = EdamamApiWrapper.list_recipes
+  end
+
+  # def show
+  #   @recipe =
+  # end
+
+  def new; end
+
+  def create # this is for searching
+    
+  end
+
 end

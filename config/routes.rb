674d8ffe0b93c58c6b6a785fe68b322251cb8117
Nora Peters <norapeters5@gmail.com>
2018-05-06 22:42:55 -0700

    removes unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
   get 'search/index', to: 'search#index', as: 'recipes'
   get 'search/:id/show', to: 'search#show', as: 'recipe'
 
-  # resources :search, only: [:index, :show]
 
-  #
-  # get 'search/new'
-  # get 'search/index'
-  # get 'search/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

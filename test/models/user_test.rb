ee1f815fd0f1891420ee93b9ef25c250dd85aa02
SelamawitA <Selamawit.ainalem@gmail.com>
2018-05-07 11:40:54 -0700

    updated tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,16 @@
 require "test_helper"
+require 'pry'
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { User.new(username:"jay-z",email:"jayz@gmail.com", uid: 132123) }
 
   it "must be valid" do
     value(user).must_be :valid?
+  end
+
+  it "must describe a user w/o a username as not valid" do
+    user[:username] = nil
+    status = user.valid?
+    status.must_equal false
   end
 end

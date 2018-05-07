4690647e37029809b86a8bbdfc0f4b3ac4d75aa1
Dikla <dikla.aharoni@gmail.com>
2018-05-06 21:43:08 -0700

    Fixed controller tests

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,21 +1,33 @@
 require "test_helper"
 
 describe RecipesController do
-  it "can get the index path" do
-    get recipes_path
 
-    must_respond_with :more
+  it "can get the index path" do
+    VCR.use_cassette("recipes") do
+      get recipes_path, params: {message: "chicken"}
+      must_respond_with :success
+    end
   end
+
+  it 'fails with nonsensical input' do
+      VCR.use_cassette("recipes") do
+        proc {get recipes_path, params: {food: "hello"} }. must_raise
+      end
+    end
+
 
   it "can get the main path" do
     get root_path
 
-    must_respond_with :more
+    must_respond_with :success
   end
 
   it "can get the show path" do
-    post recipes_path
+    VCR.use_cassette("recipes") do
+        uri = '9b5945e03f05acbf9d69625138385408'
 
-    must_respond_with :more
+        get recipe_path(uri)
+        must_respond_with :success
+      end
   end
 end

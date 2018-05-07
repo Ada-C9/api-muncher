911e0cb8e979ae2c527473b2a1f0a19e6e6cf17c
Dikla <dikla.aharoni@gmail.com>
2018-05-06 21:46:30 -0700

    Added test to the controller

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,20 +1,20 @@
 require "test_helper"
 
 describe RecipesController do
-
-  it "can get the index path" do
-    VCR.use_cassette("recipes") do
-      get recipes_path, params: {message: "chicken"}
-      must_respond_with :success
+  describe 'index' do
+    it "can get the index path" do
+      VCR.use_cassette("recipes") do
+        get recipes_path, params: {message: "chicken"}
+        must_respond_with :success
+      end
     end
-  end
 
-  it 'fails with nonsensical input' do
+    it 'fails with nonsensical input' do
       VCR.use_cassette("recipes") do
         proc {get recipes_path, params: {food: "hello"} }. must_raise
       end
     end
-
+  end
 
   it "can get the main path" do
     get root_path
@@ -22,12 +22,21 @@ describe RecipesController do
     must_respond_with :success
   end
 
-  it "can get the show path" do
-    VCR.use_cassette("recipes") do
+  describe 'show' do
+    it "can get the show path" do
+      VCR.use_cassette("recipes") do
         uri = '9b5945e03f05acbf9d69625138385408'
 
         get recipe_path(uri)
         must_respond_with :success
       end
+    end
+
+    it "fails with an invalid search" do
+      VCR.use_cassette("recipes") do
+        proc {
+          get recipe_path(bla) }.must_raise
+        end
+      end
+    end
   end
-end

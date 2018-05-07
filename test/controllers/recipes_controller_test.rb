3392c7ea2b934602e3a31f19f061caafe4bcc188
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-06 18:39:45 -0700

    added new test files

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 describe RecipesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
+  it "can get the index path" do
+    get root_path
+
+    must_respond_with :ok
+  end
+
+  # it "can get the new message form" do
+  #   get chat_new_path("PLACEHOLDER")
+	#
+  #   must_respond_with :ok
+  # end
+
+  # it "can send a message" do
+  #   post chat_create_path("PLACEHOLDER")
+	#
+  #   must_redirect_to root_path
   # end
 end

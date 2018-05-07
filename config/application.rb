75f91e56efc88cf8ae6fbb3abd0e5f404e47c5c1
Leticia Tran <leticiastran@gmail.com>
2018-05-06 18:14:16 -0700

    Fixed changes that needed to be done after adding some style to 'card'

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ApiMuncher
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib)
+    # config.autoload_paths += %W(#{config.root}/lib)
     config.eager_load_paths += %W(#{config.root}/lib)
 
     config.generators do |g|

919260f452d2148b92eb88c8d88f3ab7fa68fea8
0velez0 <mariaivelez@gmail.com>
2018-05-07 10:29:28 -0700

    create recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,32 @@
 class RecipesController < ApplicationController
 
-  def homepage
-    # this stays empty
-  end
+  def homepage; end
 
   def index
-    
+    recipes = EdamamApiWrapper.all_recipes(params[:user_input])
+
+    @recipes = Kaminari.paginate_array(recipes).page(params[:page]).per(12)
+
+    if @recipes == []
+      message = "Nothing found while searching #{params[:user_input]} Please try a different search term!"
+      status(:error, message)
+      puts "Sorry. No recipes found for #{params[:user_input]}"
+    else
+      puts "Found recipes!"
+      message = "Results found for #{params[:user_input]}"
+      status(:success, message)
+    end
   end
 
   def show
-
+    @recipe = EdamamApiWrapper.one_recipe(params[:id])
   end
 
+private
+
+  def status_message(status, message)
+      flash[:status] = status
+      flash[:result_text] = message
+  end
 
 end

3d69307e96b4be779b6857f0ebb2664b8faa1700
Samantha Berk <sberk10@gmail.com>
2018-05-06 17:03:54 -0700

    add flash messages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,10 @@ class RecipesController < ApplicationController
 
   def index
     search_term = params[:search_term]
+    if !@recipes
+      flash[:error] = "There are no recipes matching this query. Please try again."
+      render :root
+    end
     @recipes = MuncherApiWrapper.search_recipes(search_term).paginate(:page => params[:page], :per_page => 12)
     # raise
   end
@@ -17,6 +21,7 @@ class RecipesController < ApplicationController
   def show
     @recipe = MuncherApiWrapper.show_recipe(params[:format])
     if !@recipe
+      flash[:error] = "This recipe does not exist. Please try again."
       redirect_to root_path
     end
   end

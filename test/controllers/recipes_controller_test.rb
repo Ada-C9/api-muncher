a4fa400bdc08e60625d5099a6511cb4cf6eff2f4
LetiTran <leticiastran@gmail.com>
2018-05-07 01:10:46 -0700

    Finished testing controller

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -10,24 +10,32 @@ describe RecipesController do
         must_respond_with :success
       end
     end
-
-    it 'Should ' do
-
-    end
-
   end
 
   describe 'show' do
+
+    it 'Should find adn initialize a Recipe object given a specific id' do
+      VCR.use_cassette("recipes") do
+
+        teriyaki_chicken_id = "7bf4a371c6884d809682a72808da7dc2"
+
+        get recipe_path(teriyaki_chicken_id)
+
+        must_respond_with :success
+      end
+    end
+
+
     it "should render 404 for bogus recipe id and redirect to root page" do
-       VCR.use_cassette("controller") do
-         # recipe = EdamamApiWrapper.find("nil")
-         get recipe_path("nil")
+      VCR.use_cassette("controller") do
 
-         flash[:alert].must_equal "Invalid recipe."
-         must_respond_with :redirect
-         must_redirect_to root_path
-       end
+        get recipe_path("nil")
 
-     end
+        flash[:alert].must_equal "Invalid recipe."
+        must_respond_with :redirect
+        must_redirect_to root_path
+      end
+
+    end
   end
 end

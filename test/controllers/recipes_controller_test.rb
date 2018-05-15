3b0bed7e9621bfcd902b11cbe3614bb79e5e2600
Abiaina <abinnet.a@gmail.com>
2018-05-15 16:06:17 -0700

    updated wrapper test to account for nil return and array of zero values for invalid wrapper input for api request for recipes. still needs wrapper testing for recipe show page

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -50,6 +50,18 @@ describe RecipesController do
         must_redirect_to root_path
       end
     end
+
+    it "should redirect to root with invalid page and q value in params" do
+      VCR.use_cassette("recipes") do
+        get search_path, params: {
+          q: nil ,
+          page: -1
+        }
+
+        must_respond_with :redirect
+        must_redirect_to root_path
+      end
+    end
   end
 
   describe "show" do
@@ -57,83 +69,80 @@ describe RecipesController do
     it "should sucessfully show recipe details with valid data in params" do
       VCR.use_cassette("recipes") do
         get recipe_path, params: {
-          q: "avocado" ,
           r: "http://www.edamam.com/ontologies/edamam.owl#recipe_b7ad27e3e2c9440baca6ac8f7fac2a53"
         }
       end
+
       must_respond_with :success
     end
 
     it "should redirect to root with invalid r (uri) value in params" do
-
       VCR.use_cassette("recipes") do
         get recipe_path, params: {
-          q: "avocado" ,
           r: nil
         }
       end
+
       must_respond_with :redirect
       must_redirect_to root_path
+    end
+  end
+
+
+  describe "ApiMuncherWrapper.get_recipes" do
+
+    describe "get_recipes" do
+
+      it "Can get recipes with valid query and page number" do
+        VCR.use_cassette("recipes") do
+          query = "avocado"
+          page = 0
+
+          data = ApiMuncherWrapper.get_recipes(query, page)
+
+          recipe_count = data[0].to_i
+          recipes = data[2]
+
+          recipe_count.nil?.must_equal false
+
+          recipes[0].must_be_instance_of Recipe
+
+          recipes[9].must_be_instance_of Recipe
+        end
+      end
+
+      it "Returns an empty array of recipes and 0 recipe_count for recipe matches for invalid query" do
+        VCR.use_cassette("recipes") do
+          query = 999999
+          page = 0
+
+          data = ApiMuncherWrapper.get_recipes(query, page)
+
+          recipe_count = data[0].to_i
+          recipes = data[2]
+
+          recipe_count.must_equal 0
+
+          recipes.count.must_equal 0
+        end
+      end
+
+      it "Returns an an empty array for invalid page number" do
+        VCR.use_cassette("recipes") do
+          query = "avocado"
+          page = -999
+
+          data = ApiMuncherWrapper.get_recipes(query, page)
+
+          data.nil?.must_equal true
+        end
+      end
+    end
+
+    describe "get_recipe" do
 
     end
 
   end
-
-
-  # describe "ApiMuncherWrapper" do
-  #
-  #   # This code goes where an API call would be made
-  #   # Use the cassetee called channels to
-  #   it "Can get recipes with valid query and page number" do
-  #     VCR.use_cassette("recipes") do
-  #       query = "avocado"
-  #       page = 0
-  #
-  #       data = ApiMuncherWrapper.get_recipes(query, page)
-  #
-  #       recipe_count = data[0].to_i
-  #       recipes = data[2]
-  #
-  #       recipe_count.nil?.must_equal false
-  #
-  #       recipes[0].must_be_instance_of Recipe
-  #
-  #       # each call to api only returns a list of 10 recipes
-  #       recipes[9].must_be_instance_of Recipe
-  #     end
-  #   end
-  #
-  #   it "Returns an empty array of recipes and 0 recipe_count for recipe matches for invalid query" do
-  #     VCR.use_cassette("recipes") do
-  #       query = 999999
-  #       page = 0
-  #
-  #       data = ApiMuncherWrapper.get_recipes(query, page)
-  #
-  #       recipe_count = data[0].to_i
-  #       recipes = data[2]
-  #
-  #       recipe_count.must_equal 0
-  #
-  #       recipes.count.must_equal 0
-  #     end
-  #   end
-  #
-  #   it "Returns an empty array of recipes and 0 recipe_count for invalid page number" do
-  #     VCR.use_cassette("recipes") do
-  #       query = "avocado"
-  #       page = -999
-  #
-  #       data = ApiMuncherWrapper.get_recipes(query, page)
-  #
-  #       recipe_count = data[0]
-  #       recipes = data[2]
-  #
-  #       recipe_count.must_equal []
-  #
-  #       recipes.count.must_equal []
-  #     end
-  #   end
-  # end
 
 end
